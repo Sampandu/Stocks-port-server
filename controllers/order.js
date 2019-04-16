fa972d901c8d35e4fc9c9c9168e975a10410c4d6
Du <Sampan0103314@gmail.com>
2019-04-16 16:04:28 -0400

    create IEX APIcall to fetch the price of one ticker

--- controllers/order.js
+++ controllers/order.js
@@ -1,68 +1,86 @@
+const { fetchPrice } = require('./handleAPIcall')
+
 const handleOrder = (req, res, db) => {
-  const { name, ticker, quantity, price } = req.body
+  let { name, ticker, quantity } = req.body
+  ticker = ticker.toUpperCase()
 
   //input validation
   if (!Number.isInteger(quantity)) {
     return res.status(400).json('incorrect form submittion')
   }
 
-  db.select('quantity')
-    .from('portfolio')
-    .where({
-      name: name,
-      ticker: ticker
-    })
-    .then(data => {
-      console.log('0', data)
-      if(data.length === 0) {
-        return db.transaction(trx => {
-                trx.insert({name, ticker, quantity})
-                  .into('portfolio')
-                  .returning('*')
-                  .then(data => {
-                    console.log('1', data[0])
-                    return trx('transaction')
-                            .returning('*')
-                            .insert({
-                              name: data[0].name,
-                              ticker: data[0].ticker,
-                              quantity: Number(data[0].quantity),
-                              price: price
-                            })
-                            .then(tx => res.json(tx[0]))
-                  })
-                  .then(trx.commit)
-                  .catch(trx.rollback)
-              })
-              .catch(err => res.status(400).json('unable to process this order'))
+  const makeOrder = async () => {
+    try {
+      const price = await fetchPrice(req, res, ticker)
+
+      //check if the ticker exits or not
+      if(typeof price === 'string') {
+        res.status(400).json('unknown ticker')
       } else {
-        return db.transaction(trx => {
-                trx('portfolio')
-                  .where({
-                    name: name,
-                    ticker: ticker,
-                  })
-                  .increment('quantity', quantity)
-                  .returning('*')
-                  .then(data => {
-                    console.log('2', data[0])
-                    return trx('transaction')
-                            .returning('*')
-                            .insert({
-                              name: data[0].name,
-                              ticker: ticker,
-                              quantity: quantity,
-                              price: price
-                            })
-                            .then(tx => res.json(tx[0]))
-                  })
-                  .then(trx.commit)
-                  .catch(trx.rollback)
-              })
-              .catch(err => res.status(400).json('unable to process this order'))
+        // write data into db (both tables of portfolio and transaction)
+        db.select('quantity')
+          .from('portfolio')
+          .where({
+            name: name,
+            ticker: ticker
+          })
+          .then(data => {
+            // check if a user's portfolio is empty or not
+            if(data.length === 0) {
+              return db.transaction(trx => {
+                      trx.insert({name, ticker, quantity})
+                        .into('portfolio')
+                        .returning('*')
+                        .then(data => {
+                          console.log('1', data[0])
+                          return trx('transaction')
+                                  .returning('*')
+                                  .insert({
+                                    name: data[0].name,
+                                    ticker: data[0].ticker,
+                                    quantity: Number(data[0].quantity),
+                                    price: price
+                                  })
+                                  .then(tx => res.json(tx[0]))
+                        })
+                        .then(trx.commit)
+                        .catch(trx.rollback)
+                    })
+                    .catch(err => res.status(400).json('unable to process this order'))
+            } else {
+              return db.transaction(trx => {
+                      trx('portfolio')
+                        .where({
+                          name: name,
+                          ticker: ticker,
+                        })
+                        .increment('quantity', quantity)
+                        .returning('*')
+                        .then(data => {
+                          return trx('transaction')
+                                  .returning('*')
+                                  .insert({
+                                    name: data[0].name,
+                                    ticker: ticker,
+                                    quantity: quantity,
+                                    price: price
+                                  })
+                                  .then(tx => res.json(tx[0]))
+                        })
+                        .then(trx.commit)
+                        .catch(trx.rollback)
+                    })
+                    .catch(err => res.status(400).json('unable to process this order'))
+            }
+          })
+          .catch(err => res.status(400).json('unable to handle this order'))
       }
-    })
-    .catch(err => res.status(400).json('unable to handle this order'))
+      } catch(error) {
+        console.log(error)
+      }
+  }
+
+  makeOrder()
 }
 
 module.exports = { handleOrder }
