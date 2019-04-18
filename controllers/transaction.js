92634963b985a27859f88536d98b3dcf53ce53fc
Du <Sampan0103314@gmail.com>
2019-04-18 13:32:15 -0400

    fix response result of transaction and portfolio endpoint

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -5,11 +5,7 @@ const handleTransaction = (req, res, db) => {
     .from('transaction')
     .where('name', '=', name)
     .then(data => {
-      if (data.length === 0) {
-        return res.status(200).json('Your transaction is empty')
-      } else {
         return res.status(200).json(data)
-      }
     })
     .catch(err => res.status(400).json('unable to show the transaction'))
 }
