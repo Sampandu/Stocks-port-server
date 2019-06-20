a1b5486c5a0a33579a1fa8268b82e53e3e4c1b64
Du <Sampan0103314@gmail.com>
2019-06-20 10:56:30 -0400

    validate the email in the register endpoint

--- controllers/register.js
+++ controllers/register.js
@@ -8,25 +8,36 @@ const handleRegister = (req, res, db, bcrypt) => {
 
   const hash = bcrypt.hashSync(password);
 
-  db.transaction(trx => {
-    trx
-      .insert({ email, hash })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            name: name,
-            email: loginEmail[0],
-            balance: 5000,
-            joined: new Date(),
-          })
-          .then(user => res.json(user[0]));
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch(err => res.status(400).json('unable to register'));
+  db.select('*')
+    .from('login')
+    .where('email', '=', email)
+    .then(data => {
+      //validate the email
+      if (data.length) {
+        return res.status(200).json({});
+      } else {
+        db.transaction(trx => {
+          trx
+            .insert({ email, hash })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+              return trx('users')
+                .returning('*')
+                .insert({
+                  name: name,
+                  email: loginEmail[0],
+                  balance: 5000,
+                  joined: new Date(),
+                })
+                .then(user => res.json(user[0]));
+            })
+            .then(trx.commit)
+            .catch(trx.rollback);
+        }).catch(err => res.status(400).json('unable to register'));
+      }
+    })
+    .catch(err => res.status(400).json('unable to register'));
 };
 
 module.exports = { handleRegister };
